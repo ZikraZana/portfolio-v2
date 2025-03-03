c07eb57dc6f6bb944248acbafd7676ae532149ef
MonoZikk <monozikk13@gmail.com>
2025-03-03 20:53:08 +0700

    Initialization start

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Navbar from "@/component/Navbar/Navbar";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = ()=>{
+    return(
+        <div className="p-5 bg-navbar">
+            MonoZikk
+        </div>
+    )
+}
+
+export default Navbar
