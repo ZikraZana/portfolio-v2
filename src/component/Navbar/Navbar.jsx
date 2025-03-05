3194508f7677223114947bde11482ab5a81e0dc4
MonoZikk <monozikk13@gmail.com>
2025-03-05 14:36:33 +0700

    complete the navbar, then create a basic home by introducing name, profile picture and social media.

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,16 +1,15 @@
 import Link from "next/link"
 
-
 const Navbar = () => {
     return (
-        <div className="p-5 bg-navbar">
-            <nav className="flex justify-between">
+        <div className="w-full fixed text-black">
+            <nav className="p-5 flex justify-between">
                 <div className="">
                     <h1 className="text-poppins font-bold">MonoZikk</h1>
                 </div>
                 <div className="">
                     <ul className="flex gap-5">
-                        <li>
+                        <li className="">
                             <Link href={"/"}>
                                 Home
                             </Link>
